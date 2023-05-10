8ad34f68d9ba8893030b9b5b6823dbafa2e8bdab
Joao Lucas <joaolribeiro0811@gmail.com>
2023-05-09 22:55:31 -0300

    GET lists

---
+++ src/main/java/com/lucadev/dslist/controllers/GameListController.java
@@ -0,0 +1,25 @@
+package com.lucadev.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucadev.dslist.dto.GameListDTO;
+import com.lucadev.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+	@Autowired
+	private GameListService gameListService;
+	
+	@GetMapping
+	public List<GameListDTO> findAll() {
+		List<GameListDTO> result = gameListService.findAll();
+		return result;
+	}
+}
---
+++ src/main/java/com/lucadev/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.lucadev.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucadev.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+}
